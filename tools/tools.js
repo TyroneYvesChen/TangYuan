26b6c34aa5e0c8671dc3766f5b3e4d2540ab6107
TyroneYvesChen <243307272@qq.com>
2017-08-12 15:39:29 +0800

    事件绑定兼容处理addEvent

--- tools/tools.js
+++ tools/tools.js
@@ -244,6 +244,18 @@
         }
 
 
+	//事件绑定兼容处理
+        function addEvent(obj,type,handle){
+		try{ // Chrome、FireFox、Opera、Safari、IE9.0及其以上版本
+			obj.addEventListener(type,handle,false);
+		}catch(e){
+			try{ // IE8.0及其以下版本
+				obj.attachEvent('on' + type,handle);
+			}catch(e){ // 早期浏览器
+				obj['on' + type] = handle;
+			}
+		}
+        }
 
 
 
