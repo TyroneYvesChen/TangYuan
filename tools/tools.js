dc7b7bac13d5bfccc677d515b7ccc51ef3e247b8
TyroneYvesChen <243307272@qq.com>
2017-08-16 11:18:15 +0800

    //选取(picking)和反选(rejecting)对象的属性

--- tools/tools.js
+++ tools/tools.js
@@ -259,4 +259,43 @@
 
 
 
+	//选取(picking)和反选(rejecting)对象的属性
+	function pick(obj, keys) {
+	    return keys.map(k => k in obj ? {[k]: obj[k]} : {})
+		       .reduce((res, o) => Object.assign(res, o), {});
+	}
+
+
+	/**
+         const row = {
+	    'accounts.id': 1,
+	    'client.name': 'John Doe',
+	    'bank.code': 'MDAKW213'
+	};
+
+	const table = [
+	    row,
+	    {'accounts.id': 3, 'client.name': 'Steve Doe', 'bank.code': 'STV12JB'}
+	];
+
+	pick(row, ['client.name']); // 取到了 client name
+
+	table.map(row => pick(row, ['client.name'])); // 取到了一系列 client name
+         */
+	
+	function reject(obj, keys) {
+	    return Object.keys(obj)
+		.filter(k => !keys.includes(k))
+		.map(k => ({[k]: obj[k]}))
+		.reduce((res, o) => Object.assign(res, o), {});
+	}
+
+	// 或者, 利用 pick
+// 	function reject(obj, keys) {
+// 	    const vkeys = Object.keys(obj)
+// 		.filter(k => !keys.includes(k));
+// 	    return pick(obj, vkeys);
+// 	}
+
+	//reject({a: 2, b: 3, c: 4}, ['a', 'b']); // => {c: 4}
 
