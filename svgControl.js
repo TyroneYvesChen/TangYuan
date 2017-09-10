afeb82c1cad4ae8852cea8992c3e64eb4bfd6c09
TyroneYvesChen <243307272@qq.com>
2017-09-10 23:29:57 +0800

    svgControl

--- svgControl.js
+++ svgControl.js
@@ -12,13 +12,63 @@
 
     var _svgControl = SvgControl.prototype;
 
-
+    // SVG缩放平移////////////////////////////////////////////
+    // 操作SVG缩放,in->-0.1,out->0.1;in-fasle,out-true
     _svgControl.operationSvgScale = function (svg){
         var svgDom = $(svg).
         scale = svgDom.data("zoom");
         if (scale <= 0.1) return;
         var newScale = svgScale(svgDom, scale, step);
         svgDom.data("zoom", scale);
+    }
+
+    // svg平移
+    _svgControl.svgPan = function (svgContainer, isMulti){
+        svgContainer.mousedown(function(event) {
+            if (isMulti) {
+                $(this).parents(".widget-container-span").sortable('disable');
+            }
+            $(this).css("cursor",
+                "url(" + window.basePath + "/data/openhand.png), default");
+
+            var svg = $(this).find("svg");
+            var startX, startY, movingX, movingY, endX, endY;
+            startX = event.pageX;
+            startY = event.pageY;
+
+            var scale = svg.data("zoom");
+            var translate = svg.find("g#MapElement").attr("transform");
+            var panScale = svg.attr("displayscale");
+
+            $(this).bind("mousemove", function(evt) {
+                movingX = evt.pageX;
+                movingY = evt.pageY;
+                endX = (movingX - startX) * panScale;
+                endY = (movingY - startY) * panScale;
+
+                endX = parseInt(moveX);
+                endY = parseInt(moveY);
+
+                var newtranslate = panAndScale(endX, endY, scale, translate);
+                svg.find("#Backgroundcolor").attr("transform", newtranslate);
+                svg.find("g#MapElement").attr("transform", newtranslate);
+
+            });
+        });
+        svgContainer.mouseup(function(event) {
+            $(this).css("cursor", "default");
+            $(this).unbind("mousemove");
+            if (isMulti) {
+                $(this).parents(".widget-container-span").sortable('enable');
+            }
+        });
+        svgContainer.mouseout(function() {
+            $(this).css("cursor", "default");
+            $(this).unbind("mousemove");
+            if (isMulti) {
+                $(this).parents(".widget-container-span").sortable('enable');
+            }
+        });
     }
 
 
@@ -63,9 +113,9 @@
         return scale;
     }
     // svg恢复初始状态
-    function resetSVGZoom(svg, mapzoom) {
+    function resetSVGScale(svg, mapzoom) {
         // var zoom = mapzoom.data("zoom");
-        var newtranslate = panandZoom(0, 0, 1, undefined);
+        var newtranslate = panAndScale(0, 0, 1, undefined);
         svg.find("#Backgroundcolor").attr("transform", newtranslate);
         svg.find("g#MapElement").attr("transform", newtranslate);
         mapzoom.data("zoom", 1);
@@ -86,53 +136,4 @@
     }
 
 
-    // svg平移
-    function svgPan(svgContainer, isMulti) {
-        svgContainer.mousedown(function(event) {
-            if (isMulti) {
-                $(this).parents(".widget-container-span").sortable('disable');
-            }
-            $(this).css("cursor",
-                "url(" + window.basePath + "/data/openhand.png), default");
-
-            var svg = $(this).find("svg");
-            var startX, startY, movingX, movingY, endX, endY;
-            startX = event.pageX;
-            startY = event.pageY;
-
-            var zoom = svg.parents(".widget-box").find(".ale-mapzoom").data(
-                "zoom");
-            var translate = svg.find("g#MapElement").attr("transform");
-            var panScale = svg.attr("displayscale");
-
-            $(this).bind("mousemove", function(evt) {
-                movingX = evt.pageX;
-                movingY = evt.pageY;
-                moveX = (movingX - startX) * panScale;
-                moveY = (movingY - startY) * panScale;
-
-                moveX = parseInt(moveX);
-                moveY = parseInt(moveY);
-
-                var newtranslate = panandZoom(moveX, moveY, zoom, translate);
-                svg.find("#Backgroundcolor").attr("transform", newtranslate);
-                svg.find("g#MapElement").attr("transform", newtranslate);
-
-            });
-        });
-        svgContainer.mouseup(function(event) {
-            $(this).css("cursor", "default");
-            $(this).unbind("mousemove");
-            if (isMulti) {
-                $(this).parents(".widget-container-span").sortable('enable');
-            }
-        });
-        svgContainer.mouseout(function() {
-            $(this).css("cursor", "default");
-            $(this).unbind("mousemove");
-            if (isMulti) {
-                $(this).parents(".widget-container-span").sortable('enable');
-            }
-        });
-    }
 })(jQuery)
